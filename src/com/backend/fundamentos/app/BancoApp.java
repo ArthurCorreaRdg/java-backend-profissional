f63371ea348b8d3f28809db8ef8aa6f934bc4b43
ArthurCorreaRdg <arthur.correa.rdg@gmail.com>
2026-01-07 23:17:57 -0300

    refactor: arquitetura, separação em camadas

--- src/com/backend/fundamentos/app/BancoApp.java
+++ src/com/backend/fundamentos/app/BancoApp.java
@@ -1,4 +1,4 @@
-package com.backend.fundamentos.application;
+package com.backend.fundamentos.app;
 
 import com.backend.fundamentos.domain.ContaBancaria;
 
---
+++ src/com/backend/fundamentos/service/ContaService.java
@@ -0,0 +1,5 @@
+package com.backend.fundamentos.service;
+
+public class ContaService {
+
+}
